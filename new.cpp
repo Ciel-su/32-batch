a6b1705d8764af38b783645996e912d27976385a
Ciel-su <shtut571@gmail.com>
2024-12-10 10:12:46 +0630

    hyper

--- new.cpp
+++ new.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout<<"-----------------------"<<endl;
-    cout<<"|wwwwwwwwwwwwwwwwwwwwwwww|"<<endl;
-    cout<<"thank u"<<endl;
+    cout << "ARRRRRRRRR!" << endl;
 }
